2d4ca679e135d5df68c75ae8a1ea2a85cb8e1292
Erdajt Sopjani <120386306+ErdajtSopjani@users.noreply.github.com>
2024-08-25 22:41:39 +0200

    db connection | main structure finalized

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,17 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ErdajtSopjani/LikeMe_API/pkg/config"
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var app config.AppConfig
+var db *gorm.DB
 
 func init() {
 	app.IsProd = false
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file", err)
+	}
+}
+
+func initDB() (*gorm.DB, error) {
+	/* retrieve db variables from env */
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbSSLMode := os.Getenv("DB_SSLMODE")
+
+	/* create db connection string */
+	db, err := gorm.Open(
+		postgres.Open(
+			fmt.Sprintf(
+				"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+				dbHost, dbPort, dbUser, dbName, dbPassword, dbSSLMode)),
+		&gorm.Config{})
+
+	return db, err
 }
 
 func Run() {
@@ -31,14 +59,17 @@ func Run() {
 		}()
 	*/
 
-	go func() {
-		log.Printf("Server running on %s\n", addr)
-		//		db := getUserModelDB()
-		//		defer db.Close()
-		var db *gorm.DB
-		err := http.ListenAndServe(addr, router(db))
-		log.Fatalln(err)
-	}()
+	log.Printf("Server running on %s\n", addr)
+
+	var err error
+	db, err := initDB()
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+	log.Printf("Database connection established")
+
+	err = http.ListenAndServe(addr, router(db))
+	log.Fatal(err)
 
 }
 
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "github.com/ErdajtSopjani/LikeMe_API/cmd/api"
 
 func main() {
+	println("running main.go")
 	api.Run()
 }
