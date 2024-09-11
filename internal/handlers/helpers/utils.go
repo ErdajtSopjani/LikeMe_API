20a879ab7bf1bdf7bb05b749cf375599ac92d85c
Erdajt Sopjani <120386306+ErdajtSopjani@users.noreply.github.com>
2024-09-11 19:35:53 +0200

    test for register endpoint done - fixed RespondError to return a json object with a 'message' field - now the tests can read the 'message' correctly

--- internal/handlers/helpers/utils.go
+++ internal/handlers/helpers/utils.go
@@ -6,11 +6,15 @@ import (
 	"net/http"
 )
 
+// RespondError function to return error message as response
 func RespondError(w http.ResponseWriter, message string, status int) {
-	http.Error(w, message, status)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(map[string]string{"message": message})
 	log.Printf("Error: %s, Status: %d", message, status)
 }
 
+// RespondJSON function to return JSON message as response (mainly used on success)
 func RespondJSON(w http.ResponseWriter, status int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
